370195311802afd0b16bd25ed014af221f432305
jaadisini <162589971+jaadisini@users.noreply.github.com>
2025-07-16 18:13:15 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+API_ID = 28932643  # Ganti dengan API ID Anda
+API_HASH = "3fb38bb53a7d33fb42ea208c6f77830b"  # Ganti dengan API HASH Anda
+BOT_TOKEN = "8160674170:AAHpUzUKgdF2Y5cIuwIdhBuuft3uFZ7CCME"  # Ganti dengan BOT TOKEN Anda
