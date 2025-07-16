a2798daebea48ef72bbaeb277d3506c0f70aff42
jaadisini <162589971+jaadisini@users.noreply.github.com>
2025-07-16 18:41:09 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,6 @@ API_ID = 28932643  # Ganti dengan API ID Anda
 API_HASH = "3fb38bb53a7d33fb42ea208c6f77830b"  # Ganti dengan API HASH Anda
 BOT_TOKEN = "8160674170:AAHpUzUKgdF2Y5cIuwIdhBuuft3uFZ7CCME"  # Ganti dengan BOT TOKEN Anda
 OWNER_ID = 6305402536  # ID Telegram kamu
-LOG_CHAT_ID = -1002531425967  
+LOG_CHAT_ID = -1002531425967 
+MENTION_COOLDOWN = 300  # Detik (5 menit)
+DB_PATH = "partner_groups.db"
